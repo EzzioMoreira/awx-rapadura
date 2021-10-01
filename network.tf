324097647a1258d6ae19d0746574e102dba32504
enzziom <enzziom@gmail.com>
2021-10-01 19:02:03 -0300

    Ajudeste output.tf

--- network.tf
+++ network.tf
@@ -18,7 +18,7 @@ resource "aws_security_group" "subnet" {
       from_port   = port.value
       to_port     = port.value
       protocol    = "tcp"
-      cidr_blocks = [data.aws_subnet.selected.cidr_block]
+      cidr_blocks = ["0.0.0.0/0"]
     }
   }
 
@@ -29,7 +29,7 @@ resource "aws_security_group" "subnet" {
       from_port   = port.value
       to_port     = port.value
       protocol    = "tcp"
-      cidr_blocks = [data.aws_subnet.selected.cidr_block]
+      cidr_blocks = ["0.0.0.0/0"]
     }
   }
 }
--- output.tf
+++ output.tf
@@ -1,19 +1,19 @@
-output "rapadura-id" {
+output "awx-rapadura-id" {
   description = "The id of instance"
   value       = [aws_instance.devops-test.*.id]
 }
 
-output "rapadura-public_dns" {
+output "awx-rapadura-public_dns" {
   description = "The public dns of instance"
   value       = [aws_instance.devops-test.*.public_dns]
 }
 
-output "rapadura-public_ip" {
+output "awx-rapadura-public_ip" {
   description = "The public ip of instance"
   value       = [aws_instance.devops-test.*.public_ip]
 }
 
-output "rapadura-private_ip" {
+output "awx-rapadura-private_ip" {
   description = "The private ip of instance"
   value       = [aws_instance.devops-test.*.private_ip]
 }
